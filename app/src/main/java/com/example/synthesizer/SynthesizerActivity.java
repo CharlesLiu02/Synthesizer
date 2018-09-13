3a5caddacc2b1509f0472c1710c894cc1e6108b1
CharlesLiu02 <roomcool02@gmail.com>
2018-09-13 08:01:26 -0700

    a lot of stuff

--- app/src/main/java/com/example/synthesizer/SynthesizerActivity.java
+++ app/src/main/java/com/example/synthesizer/SynthesizerActivity.java
@@ -1,13 +1,24 @@
 package com.example.synthesizer;
 
+import android.content.DialogInterface;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
-public class SynthesizerActivity extends AppCompatActivity implements View.OnClickListener{
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SynthesizerActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final float DEFAULT_VOLUME = 1.0f;
     public static final int DEFAULT_PRIORITY = 1;
@@ -27,6 +38,12 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
     private Button buttonG;
     private Button buttonGsharp;
     private Button buttonPlayScale;
+    private Button buttonPlayScaleE;
+    private Button buttonChallenge2;
+    private NumberPicker numberPickerTimes;
+    private NumberPicker numberPickerNote;
+    private Map<Integer, Integer> noteMap;
+
     private SoundPool soundPool;
     private int noteA;
     private int noteBb;
@@ -41,6 +58,31 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
     private int noteG;
     private int noteGsharp;
 
+    private Button buttonHighA;
+    private Button buttonHighBb;
+    private Button buttonHighB;
+    private Button buttonHighC;
+    private Button buttonHighCsharp;
+    private Button buttonHighD;
+    private Button buttonHighDsharp;
+    private Button buttonHighE;
+    private Button buttonHighF;
+    private Button buttonHighFsharp;
+    private Button buttonHighG;
+    private Button buttonHighGsharp;
+    private int noteHighA;
+    private int noteHighBb;
+    private int noteHighB;
+    private int noteHighC;
+    private int noteHighCsharp;
+    private int noteHighD;
+    private int noteHighDsharp;
+    private int noteHighE;
+    private int noteHighF;
+    private int noteHighFsharp;
+    private int noteHighG;
+    private int noteHighGsharp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,6 +91,40 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
         wireWidgets();
         setListeners();
         initializeSoundPool();
+        initializeNoteMap();
+    }
+
+    private void initializeNoteMap() {
+        noteMap = new HashMap<>();
+        // in a map, you store a key:value pair
+        // key is button id
+        // value is note id
+        noteMap.put(R.id.button_synth_a, noteA);
+        noteMap.put(R.id.button_synth_b, noteB);
+        noteMap.put(R.id.button_synth_bb, noteBb);
+        noteMap.put(R.id.button_synth_c, noteC);
+        noteMap.put(R.id.button_synth_csharp, noteCsharp);
+        noteMap.put(R.id.button_synth_d, noteD);
+        noteMap.put(R.id.button_synth_dsharp, noteDsharp);
+        noteMap.put(R.id.button_synth_e, noteE);
+        noteMap.put(R.id.button_synth_f, noteF);
+        noteMap.put(R.id.button_synth_fsharp, noteFsharp);
+        noteMap.put(R.id.button_synth_g, noteG);
+        noteMap.put(R.id.button_synth_gsharp, noteG);
+        noteMap.put(R.id.button_synth_highA, noteHighA);
+        noteMap.put(R.id.button_synth_highB, noteHighB);
+        noteMap.put(R.id.button_synth_highBb, noteHighBb);
+        noteMap.put(R.id.button_synth_highC, noteHighC);
+        noteMap.put(R.id.button_synth_highCsharp, noteHighCsharp);
+        noteMap.put(R.id.button_synth_highD, noteHighD);
+        noteMap.put(R.id.button_synth_highDsharp, noteHighDsharp);
+        noteMap.put(R.id.button_synth_highE, noteHighE);
+        noteMap.put(R.id.button_synth_highF, noteHighF);
+        noteMap.put(R.id.button_synth_highFsharp, noteHighFsharp);
+        noteMap.put(R.id.button_synth_highG, noteHighG);
+        noteMap.put(R.id.button_synth_highGsharp, noteHighGsharp);
+
+
     }
 
     private void initializeSoundPool() {
@@ -65,27 +141,55 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
         noteFsharp = soundPool.load(this, R.raw.scalefs, 1);
         noteG = soundPool.load(this, R.raw.scaleg, 1);
         noteGsharp = soundPool.load(this, R.raw.scalegs, 1);
+
+        noteHighA = soundPool.load(this, R.raw.scalehigha, 1);
+        noteHighBb = soundPool.load(this, R.raw.scalehighbb, 1);
+        noteHighB = soundPool.load(this, R.raw.scalehighb, 1);
+        noteHighC = soundPool.load(this, R.raw.scalehighc, 1);
+        noteHighCsharp = soundPool.load(this, R.raw.scalehighcs, 1);
+        noteHighD = soundPool.load(this, R.raw.scalehighd, 1);
+        noteHighDsharp = soundPool.load(this, R.raw.scalehighds, 1);
+        noteHighE = soundPool.load(this, R.raw.scalehighe, 1);
+        noteHighF = soundPool.load(this, R.raw.scalehighf, 1);
+        noteHighFsharp = soundPool.load(this, R.raw.scalehighfs, 1);
+        noteHighG = soundPool.load(this, R.raw.scalehighg, 1);
+        noteHighGsharp = soundPool.load(this, R.raw.scalehighgs, 1);
     }
 
     private void setListeners() {
-        buttonA.setOnClickListener(this);
-        buttonBb.setOnClickListener(this);
-        buttonB.setOnClickListener(this);
-        buttonC.setOnClickListener(this);
-        buttonCsharp.setOnClickListener(this);
-        buttonD.setOnClickListener(this);
-        buttonDsharp.setOnClickListener(this);
-        buttonE.setOnClickListener(this);
-        buttonF.setOnClickListener(this);
-        buttonFsharp.setOnClickListener(this);
-        buttonG.setOnClickListener(this);
-        buttonGsharp.setOnClickListener(this);
+        KeyboardNoteListener noteListener = new KeyboardNoteListener();
+        buttonA.setOnClickListener(noteListener);
+        buttonBb.setOnClickListener(noteListener);
+        buttonB.setOnClickListener(noteListener);
+        buttonC.setOnClickListener(noteListener);
+        buttonCsharp.setOnClickListener(noteListener);
+        buttonD.setOnClickListener(noteListener);
+        buttonDsharp.setOnClickListener(noteListener);
+        buttonE.setOnClickListener(noteListener);
+        buttonF.setOnClickListener(noteListener);
+        buttonFsharp.setOnClickListener(noteListener);
+        buttonG.setOnClickListener(noteListener);
+        buttonGsharp.setOnClickListener(noteListener);
         buttonPlayScale.setOnClickListener(this);
+        buttonPlayScaleE.setOnClickListener(this);
+
+        buttonHighA.setOnClickListener(noteListener);
+        buttonHighBb.setOnClickListener(noteListener);
+        buttonHighB.setOnClickListener(noteListener);
+        buttonHighC.setOnClickListener(noteListener);
+        buttonHighCsharp.setOnClickListener(noteListener);
+        buttonHighD.setOnClickListener(noteListener);
+        buttonHighDsharp.setOnClickListener(noteListener);
+        buttonHighE.setOnClickListener(noteListener);
+        buttonHighF.setOnClickListener(noteListener);
+        buttonHighFsharp.setOnClickListener(noteListener);
+        buttonHighG.setOnClickListener(noteListener);
+        buttonHighGsharp.setOnClickListener(noteListener);
     }
 
     private void wireWidgets() {
         buttonA = findViewById(R.id.button_synth_a);
-        buttonB = findViewById(R.id.button_syth_b);
+        buttonB = findViewById(R.id.button_synth_b);
         buttonBb = findViewById(R.id.button_synth_bb);
         buttonC = findViewById(R.id.button_synth_c);
         buttonCsharp = findViewById(R.id.button_synth_csharp);
@@ -97,6 +201,21 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
         buttonG = findViewById(R.id.button_synth_g);
         buttonGsharp = findViewById(R.id.button_synth_gsharp);
         buttonPlayScale = findViewById(R.id.button_synth_scale);
+        buttonPlayScaleE = findViewById(R.id.button_synth_scaleE);
+        buttonChallenge2 = findViewById(R.id.button_synth_challenge2);
+
+        buttonHighA = findViewById(R.id.button_synth_highA);
+        buttonHighB = findViewById(R.id.button_synth_highB);
+        buttonHighBb = findViewById(R.id.button_synth_highBb);
+        buttonHighC = findViewById(R.id.button_synth_highC);
+        buttonHighCsharp = findViewById(R.id.button_synth_highCsharp);
+        buttonHighD = findViewById(R.id.button_synth_highD);
+        buttonHighDsharp = findViewById(R.id.button_synth_highDsharp);
+        buttonHighE = findViewById(R.id.button_synth_highE);
+        buttonHighF = findViewById(R.id.button_synth_highF);
+        buttonHighFsharp = findViewById(R.id.button_synth_highFsharp);
+        buttonHighG = findViewById(R.id.button_synth_highG);
+        buttonHighGsharp = findViewById(R.id.button_synth_highGsharp);
 
 
     }
@@ -105,46 +224,48 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View view) {
         // one method to handle the clicks of all the buttons
         // but don't forget to tell the buttons who is doing the listening
-        switch(view.getId()){
-            case R.id.button_synth_a:
-                soundPool.play(noteA, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_syth_b:
-                soundPool.play(noteB, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_bb:
-                soundPool.play(noteBb, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_csharp:
-                soundPool.play(noteCsharp, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_d:
-                soundPool.play(noteD, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_dsharp:
-                soundPool.play(noteDsharp, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_e:
-                soundPool.play(noteE, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_f:
-                soundPool.play(noteF, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_fsharp:
-                soundPool.play(noteFsharp, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_g:
-                soundPool.play(noteG, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_gsharp:
-                soundPool.play(noteGsharp, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
-            case R.id.button_synth_c:
-                soundPool.play(noteC, 1.0f,1.0f, 1, 0, 1.0f);
-                break;
+        switch (view.getId()) {
             case R.id.button_synth_scale:
                 playScale();
                 break;
+            case R.id.button_synth_scaleE:
+                playScaleE();
+                break;
+            case R.id.button_synth_challenge2:
+                promptResponse();
+
+        }
+    }
+
+    private void promptResponse() {
+        numberPickerNote = new NumberPicker(this);
+        numberPickerTimes = new NumberPicker(this);
+
+        numberPickerTimes.set
+
+    }
+
+    private void playScaleE() {
+        Song scale = new Song();
+
+        scale.add(new Note(noteE, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteFsharp, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteG, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteHighA, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteHighB, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteHighCsharp, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteHighD, Note.WHOLE_NOTE / 2));
+        scale.add(new Note(noteHighE, Note.WHOLE_NOTE / 2));
+
+        playSong(scale);
+
+
+    }
+
+    private void playSong(Song scale) {
+        for(Note note : scale.getNotes()){
+            playNote(note);
+            delay(note.getDelay());
         }
     }
 
@@ -172,6 +293,30 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
         playNote(noteG);
         delay(WHOLE_NOTE);
         playNote(noteGsharp);
+        delay(WHOLE_NOTE);
+        playNote(noteHighA);
+        delay(WHOLE_NOTE);
+        playNote(noteHighBb);
+        delay(WHOLE_NOTE);
+        playNote(noteHighB);
+        delay(WHOLE_NOTE);
+        playNote(noteHighC);
+        delay(WHOLE_NOTE);
+        playNote(noteHighCsharp);
+        delay(WHOLE_NOTE);
+        playNote(noteHighD);
+        delay(WHOLE_NOTE);
+        playNote(noteHighDsharp);
+        delay(WHOLE_NOTE);
+        playNote(noteHighE);
+        delay(WHOLE_NOTE);
+        playNote(noteHighF);
+        delay(WHOLE_NOTE);
+        playNote(noteHighFsharp);
+        delay(WHOLE_NOTE);
+        playNote(noteHighG);
+        delay(WHOLE_NOTE);
+        playNote(noteHighGsharp);
     }
 
     private void delay(int duration) {
@@ -189,5 +334,25 @@ public class SynthesizerActivity extends AppCompatActivity implements View.OnCli
     private void playNote(int note, int loop) {
         soundPool.play(note, DEFAULT_VOLUME, DEFAULT_VOLUME, DEFAULT_PRIORITY, loop, DEFAULT_RATE);
     }
+
+    private void playNote(Note note){
+        playNote(note.getNoteID(), 0);
+    }
+
+    // make an inner class to handle the button clicks for individual notes
+
+    private class KeyboardNoteListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            // get the id of the button that was clicked
+            int id = view.getId();
+            // use the map to figure out what note to play
+            int note = noteMap.get(id);
+            // play the note
+            playNote(note);
+        }
+    }
+
 
 }
